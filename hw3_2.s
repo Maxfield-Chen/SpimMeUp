eb34b9298cf56ef2bf394476e39053b11af5232f
joeshvedsky <shvedj@rpi.edu>
2015-03-16 21:23:52 -0400

    final commit

--- hw3_2.s
+++ hw3_2.s
@@ -15,6 +15,10 @@ error:	.asciiz "Line contains whitespace characters only!\n"
 	.text
 	.globl main
 main:
+	li $t1, 32 #single whitespace
+	li $t2, 9 #tab
+	li $t3, 10 #newline
+	
 	#print the input prompt
 	li $v0, 4
 	la $a0, input_prompt
@@ -26,6 +30,27 @@ main:
 	li $a1, 100
 	move $s0, $a0  #save the input string to $s0
 	syscall
+	
+	move $t4, $s0
+	#check if only whitespace
+	check_input:
+		lb $t5, 0($t4)
+		addi $t4, $t4, 1
+		beq $t5, $t1, check_input
+		beq $t5, $t2, check_input
+		beq $t5, $t3, check_input
+		beq $t5, 0, print_error
+
+	j read_pattern
+	
+	print_error:
+		li $v0, 4
+		la $a0, error
+		syscall
+		li $v0, 10
+		syscall #to the batmobile
+		
+	read_pattern:
 	
 	#print the pattern prompt
 	li $v0, 4
@@ -44,18 +69,105 @@ main:
 	li $s2, 0 #whitespace count
 	li $s3, 0 #non-whitespace count
 	li $s4, 0 #word count
-	li $t1, 32 #single whitespace
-	li $t2, 9 #tab
-	li $t3, 10 #newline
-
+	move $s5, $s1 #for going to start of pattern
+	li $t4, 0 #temp variable for word counting (seen_char)
+	li $t5, 0 #temp variable for pattern matching (seen_pattern)
+	
 	input_loop:
 		lb $t0, 0($s0) #load each successive input character
+		addi $s0, $s0, 1		
+		beq $t0, 0, exit_loop
 		beq $t0, $t1, increment_whitespace
 		beq $t0, $t2, increment_whitespace
 		beq $t0, $t3, increment_whitespace
+		li $t4, 1
 		addi $s3, $s3, 1
+	call_pattern:
+		jal pattern_match
+		j input_loop	
+	
+	increment_whitespace:
+		addi $s2, $s2, 1
+		beq $t4, 1, increment_word
+		j call_pattern
+	
+	increment_word:
+		move $t4, $0
+		addi $s4, $s4, 1
+		j call_pattern
+	
+	pattern_match:
+		beq $t5, 1, exit_pattern
+		lb $t6, 0($s1)
+		beq $t6, 10, pattern_matched
+		beq $t6, $t0, increment_pattern_char
+		move $s1, $s5
+		jr $31
+	
+	exit_pattern:
+		jr $31
+	
+	pattern_matched:	
+		li $t5, 1
+		jr $31
 		
-		increment_whitespace:
-			addi $s2, $s2, 1	
+	increment_pattern_char:
+		addi $s1, $s1, 1
+		jr $31
+	
+	exit_loop:
+	
+	li $v0, 4
+	la $a0, results
+	syscall
+	
+	li $v0, 4
+	la $a0, whitespace
+	syscall
+	
+	li $v0, 1
+	move $a0, $s2
+	syscall
+	
+	li $v0, 4
+	la $a0, new_line
+	syscall
+	
+	li $v0, 4
+	la $a0, non_whitespace
+	syscall
+	
+	li $v0, 1
+	move $a0, $s3
+	syscall
+	
+	li $v0, 4
+	la $a0, new_line
+	syscall
+	
+	li $v0, 4
+	la $a0, words
+	syscall
+	
+	li $v0, 1
+	move $a0, $s4
+	syscall 
+	
+	li $v0, 4
+	la $a0, new_line
+	syscall
+	
+	li $v0, 4
+	beq $t5, 1 print_found
+	
+	la $a0, not_found
+	syscall
+	
 	li $v0, 10
 	syscall
+	
+	print_found:
+		la $a0, found
+		syscall 
+		li $v0, 10
+		syscall
